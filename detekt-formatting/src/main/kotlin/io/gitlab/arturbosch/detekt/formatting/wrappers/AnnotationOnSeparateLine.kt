6c931f78c47eae37ff0401f3b6d1dfb250bbb1ba
Artur Bosch <arturbosch@gmx.de>
2019-02-17 13:02:25 +0100

    Disable AnnotationOnSeparateLine and SpacingAroundDot by default due to false positives and exceptions

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/AnnotationOnSeparateLine.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/AnnotationOnSeparateLine.kt
@@ -7,12 +7,10 @@ import io.gitlab.arturbosch.detekt.formatting.FormattingRule
 /**
  * See <a href="https://ktlint.github.io">ktlint-website</a> for documentation.
  *
- * @active since v1.0.0
- * @autoCorrect since v1.0.0
  * @author Artur Bosch
  */
 class AnnotationOnSeparateLine(config: Config) : FormattingRule(config) {
 
-	override val wrapping = AnnotationRule()
-	override val issue = issueFor("Multiple annotations should be placed on separate lines. ")
+    override val wrapping = AnnotationRule()
+    override val issue = issueFor("Multiple annotations should be placed on separate lines. ")
 }
